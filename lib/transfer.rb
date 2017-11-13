ba3594b4120d47f8c1badb1fb0339bca019a8a22
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 22:07:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
     if(status == "complete")
       receiver.balance -= @amount
       sender.deposit(@amount)
-      @status = "complete"
+      @status = "reversed"
     end
   end
 
