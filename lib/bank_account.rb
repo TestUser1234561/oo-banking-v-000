66224d56388532b3222f3257a32f6f6b658022a5
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 21:52:48 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,16 @@
 class BankAccount
 
-  attr_accessor :name, :balance, :status
+  attr_accessor :balance, :status
+  @name
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
+  end
+
+  def name
+    @name
   end
 
 end
