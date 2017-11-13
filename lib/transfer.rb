b91eb86d5ef235cee5592558e659ffd4b3bf1ca4
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 22:01:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,9 +15,11 @@ class Transfer
 
   def execute_transaction
     return "Transaction rejected. Please check your account balance." if !self.valid?
-    sender.balance -= @amount
-    receiver.deposit(@amount)
-    @status = "complete"
+    if(status == "pending")
+      sender.balance -= @amount
+      receiver.deposit(@amount)
+      @status = "complete"
+    end
   end
 
 end
